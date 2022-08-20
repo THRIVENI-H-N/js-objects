258be30b6a1606db8036b61764726f73c9b38de1
THRIVENI-H-N <107698532+THRIVENI-H-N@users.noreply.github.com>
2022-08-20 21:25:18 +0530

    Add files via upload

---
+++ check-obj-para.js
@@ -0,0 +1,8 @@
+function Check(obj1) {
+    for(var prop in obj1) {
+                 if(obj1.hasOwnProperty(prop))
+                     return "true";
+             }
+         
+             return "false";
+   }
---
+++ sum-value.js
@@ -0,0 +1,4 @@
+function Check(obj1) {
+    return Object.values(obj1).reduce((a, b) => a + b, 0);
+    
+   }
